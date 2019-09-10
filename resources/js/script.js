a42e0901541ab738f93e54699bfaa9c9c7e46b86
LebohM <46931086+LebohM@users.noreply.github.com>
2019-09-10 15:54:00 +0200

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+
+
+  /* For the sticky navigation */
+  $('.js--section-features').waypoint(function(direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  }, {
+    offset: '60px;'
+  });
+
+  // button scrolling
+  $('.js--scroll-to-plan').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.js--section-plans').offset().top
+    }, 1000);
+  });
+
+  $('.js--scroll-to-start').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.js--section-features').offset().top
+    }, 1000);
+  });
+
+  // smooth scrolling
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+  /* Animations on scroll */
+  $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').addClass('animated fadeIn');
+  }, {
+    offset: '60%'
+  });
+
+  $('.js--wp-5').waypoint(function(direction) {
+    $('.js--wp-5').addClass('animated pulse');
+  }, {
+    offset: '40%'
+  });
+
+  /* Mobile navigation */
+  $('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass('ion-navicon')) {
+      icon.addClass('ion-ios-close-outline');
+      icon.removeClass('ion-navicon');
+    } else {
+      icon.addClass('ion-navicon');
+      icon.removeClass('ion-ios-close-outline');
+    }
+  });
+
+});
